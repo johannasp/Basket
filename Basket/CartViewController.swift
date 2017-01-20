3e3b1371eab72dd6ec78cc11fb930ea4f5696c97
Johanna Smith-Palliser <godinas@Johannas-MacBook-Pro.local>
2017-01-19 22:10:26 -0800

    let's hope this works

--- Basket/CartViewController.swift
+++ Basket/CartViewController.swift
@@ -13,8 +13,6 @@ class CartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "screenshot.png")!)
-        
         let background = UIImage(named: "screenshot.png")
         
         var imageView : UIImageView!
@@ -25,10 +23,33 @@ class CartViewController: UIViewController {
         imageView.center = view.center
         view.addSubview(imageView)
         self.view.sendSubview(toBack: imageView)
-
         
-        
+        let groupOrderButton = UIButton(frame: CGRect(x: 0, y: 475, width: 330, height: 48))
+        groupOrderButton.center.x = view.center.x
+        groupOrderButton.setBackgroundImage(UIImage(named: "group order button.png"), for: .normal)
+        groupOrderButton.addTarget(self, action: #selector(goToGroupOrderViewController), for: .touchUpInside)
+        view.addSubview(groupOrderButton)
     }
-
+ 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+ 
+    func goToGroupOrderViewController() {
+        navigationController?.pushViewController(GroupOrderViewController(), animated: true)
+        print("test")
+    }
+    
+    // Remove navigation bar from login/sign up screen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    // Put back navigation bar once you leave this view so it shows up on all other views
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(false)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
 }
-
