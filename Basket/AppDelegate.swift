d7cdef749ccf106a7c3e00be56f7fbbeb6250019
Johanna Smith-Palliser <godinas@Johannas-MacBook-Pro.local>
2017-01-19 21:12:06 -0800

    I'm committin'

---
+++ Basket/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Basket
+//
+//  Created by Johanna Smith-Palliser on 1/19/17.
+//  Copyright © 2017 Johanna Smith-Palliser. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        FIRApp.configure()
+        
+        window = UIWindow()
+        window?.rootViewController = CartViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Basket/CartViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Basket
+//
+//  Created by Johanna Smith-Palliser on 1/19/17.
+//  Copyright © 2017 Johanna Smith-Palliser. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "screenshot.png")!)
+        
+        let background = UIImage(named: "screenshot.png")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
+
+        
+        
+    }
+
+}
+
